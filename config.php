d9c016b8e8dbf11e9f852125837c45842e3b40e7
erdyputra22 <61159398+erdyputra22@users.noreply.github.com>
2021-02-13 10:39:52 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 9; Redmi Note 5 Build/PQ3A.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.132 Mobile Safari/537.36 Shopee Beeshop locale/id version=393 appver=26132 rnver=1613046285 app_type=1";
+$DeviceId = "xxxxxxxx";
+$SPCF = "SPC_F=022925563805862_87715402e0c0b317_CD267EA32B687C0A";
+$SPCAFTID = "SPC_AFTID=7743ac5a-7d46-4fb9-b5a6-f4f7640a87aa";
+$UserId = "userid=374920254";
+$Username = "username=rgxouyt15t";
+$CsrfToken = "csrftoken=RiW0xeYBhl35mOKta9M1ILA5l9sNO55T";
+$ShopeeToken = "shopee_token=6eBQjtSFe3WkbR9esX9ZLxeWvgoCBSX5Dca88CRi+QDsdq/aybBkU3uGk1k/4x1n";
+$SPCEC = 'SPC_EC=RZaGr2t6sFgi5k9Sr0MEZNkyUt5YVQQPGQKZEgcQfg9ah1QNe0U+bj6s/onaefh4HfZyXqnGbEaK41XAfwj9PSIELx8bxMIGxeRcURVGZx/G9NVuXelpxnnbkGYb9qOenEUd33zTOTOlx3elZ5eb621gCMG6Lr72Cr+YPsxsM6A=';
